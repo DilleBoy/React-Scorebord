402497875aa95ec3c230a4ec705f61e0b99ec76a
D Sadighi <DilleBoy@gmail.com>
2021-08-12 16:52:18 +0200

    Updates

---
+++ precache-manifest.bab5a04f90938a370d2e4e74666b436c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91b33bca0b03a0259fd5",
+    "url": "/React-Scorebord/static/css/main.b6c59205.chunk.css"
+  },
+  {
+    "revision": "91b33bca0b03a0259fd5",
+    "url": "/React-Scorebord/static/js/main.91b33bca.chunk.js"
+  },
+  {
+    "revision": "39a0acda35a094cf9c2c",
+    "url": "/React-Scorebord/static/js/1.39a0acda.chunk.js"
+  },
+  {
+    "revision": "90cc4dc8dd65962b4575",
+    "url": "/React-Scorebord/static/js/runtime~main.90cc4dc8.js"
+  },
+  {
+    "revision": "667ae89e2cc591eaa0aa0744f425f41b",
+    "url": "/React-Scorebord/index.html"
+  }
+];
